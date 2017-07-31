b1d3881f0e0630b4c22be9ff879374634bce7e5a
evuz <jesus_jgb_45@hotmail.com>
2017-07-31 13:30:21 +0200

    Welcome component: Add button signIn and singUp

--- src/renderer/app/components/Welcome/index.js
+++ src/renderer/app/components/Welcome/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 import './index.scss';
 
-const WelcomeComponent = () => (
+const WelcomeComponent = ({ onClickSignIn, onClickSignUp }) => (
   <div className="welcome_component">
-    <p>Welcome to Visual Comic Reader</p>
+    <p className="description">Welcome to Visual Comic Reader</p>
+    <p className="info">You can login with your mail to save your read comics</p>
+    <div className="buttons">
+      <button onClick={onClickSignIn}>Sign in</button>
+      <button onClick={onClickSignUp}>Sign up</button>
+    </div>
   </div>
 );
 
 export default WelcomeComponent;
+
