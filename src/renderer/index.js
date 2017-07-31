859122617ee05bb4d10f9200d4104227d31d39d1
evuz <jesus_jgb_45@hotmail.com>
2017-07-31 13:25:04 +0200

    Initialize firebase

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import firebase from 'firebase';
+
 import configureStore from './configureStore';
+import configFirebase from './configFirebase';
+
 import App from './app/app';
 
+firebase.initializeApp(configFirebase);
 const store = configureStore();
 
 ReactDOM.render(
